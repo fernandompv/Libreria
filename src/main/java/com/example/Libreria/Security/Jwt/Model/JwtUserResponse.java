f46b7fa6b98a05cb90c4b46f822366fb1c4f19c5
Fernando Piñeiro <fernando.pineiro@disashop.com>
2020-09-01 12:41:53 +0200

    prueba para comprobar que detecta el cambio en el código

--- src/main/java/com/example/Libreria/Security/Jwt/Model/JwtUserResponse.java
+++ src/main/java/com/example/Libreria/Security/Jwt/Model/JwtUserResponse.java
@@ -17,7 +17,7 @@ public class JwtUserResponse extends GetUsuarioDto {
 
     @Builder(builderMethodName="jwtUserResponseBuilder")
     public JwtUserResponse(String username, Set<String> roles, String token) {
-        super(username + "123456", roles);
+        super(username, roles);
         this.token = token;
     }
 
